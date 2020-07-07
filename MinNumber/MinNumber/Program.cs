ae567e5fc24d20e41d941fb628f75d62051fdaf0
Kirill <kkdeveloper7@gmail.com>
2020-07-07 15:33:39 -0400

    min number

---
+++ MinNumber/MinNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var integerSet = new List<int> { 5, 90, 44, 3, 555, 602, 8 };
+            var result = GetLowestValue(integerSet);
+            Console.WriteLine("The lowest number is: " + result);
+            Console.ReadLine();
+        }
+
+        public static int GetLowestValue(List<int> inputValues)
+        {
+            var minValue = inputValues[0];
+
+            for (var i = 0; i < inputValues.Count; i++)
+            {
+                var number = inputValues[i];
+
+                if (number < minValue)
+                {
+                    minValue = number;
+                }
+            }
+            return minValue;
+        }
+    }
+}
