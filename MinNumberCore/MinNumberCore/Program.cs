90ea984623fb7758c9bec564b5fb49216e6f3d28
Kirill <kkdeveloper7@gmail.com>
2020-07-09 12:49:50 -0400

    added exceptions

--- MinNumberCore/MinNumberCore/Program.cs
+++ MinNumberCore/MinNumberCore/Program.cs
@@ -7,31 +7,23 @@ namespace MinNumberCore
     {
         static void Main(string[] args)
         {
-            var integerSet = new List<int> { 5, 90, 44, 3, -1, 555, 602, 8 };
+            var integerSet = new List<int> { };
             var result = GetLowestValue(integerSet);
             Console.WriteLine("The lowest number is: " + result);
 
-            List<int> integerSet1 = null;
-            var result1 = GetLowestValue(integerSet1);
-            if (result1 == null)
-            {
-                Console.WriteLine("The list is null or doesn't contain any elements");
-            }
-
-            var integerSet2 = new List<int>();
-            var result2 = GetLowestValue(integerSet2);
-            if (result2 == null)
-            {
-                Console.WriteLine("The list is null or doesn't contain any elements");
-            }
             Console.ReadLine();
         }
 
-        public static int? GetLowestValue(List<int> inputValues)
+        public static int GetLowestValue(List<int> inputValues)
         {
-            if (inputValues == null || inputValues.Count == 0)
+            if (inputValues == null)
             {
-                return null;
+                throw new NullReferenceException("Input values are null");
+            }
+
+            if (inputValues.Count == 0)
+            {
+                throw new Exception("List doesn't contain any elements");
             }
 
             var minValue = inputValues[0];
